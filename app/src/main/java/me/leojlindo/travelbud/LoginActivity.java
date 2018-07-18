ed64b8dc365c2e213bf2a239952728fc719c0e3a
Leonardo Lindo <leojlindo@fb.com>
2018-07-18 15:52:12 -0700

    touched up sign up activity

--- app/src/main/java/me/leojlindo/travelbud/LoginActivity.java
+++ app/src/main/java/me/leojlindo/travelbud/LoginActivity.java
@@ -3,25 +3,52 @@ package me.leojlindo.travelbud;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText usernameInput;
+    private EditText passwordInput;
+    private Button loginBtn;
+
+    // keeps logged in users logged in
+    protected void onStart(){
+        super.onStart();
+        ParseUser currentUser = ParseUser.getCurrentUser();
+        if (currentUser != null) {
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        } else {
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        // hides the action bar
-        //getSupportActionBar().hide();
+        // variables the app needs to log in
+        usernameInput = findViewById(R.id.username_et);
+        passwordInput = findViewById(R.id.password_et);
+        loginBtn = findViewById(R.id.login_btn);
 
-        // connecting login button to our main page
-        Button login_btn = (Button) findViewById(R.id.login_btn);
+        // connecting login the user into our app
 
-        login_btn.setOnClickListener(new View.OnClickListener(){
+        loginBtn.setOnClickListener(new View.OnClickListener(){
             public void onClick(View view){
-                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                final String username = usernameInput.getText().toString();
+                final String password = passwordInput.getText().toString();
+                login(username, password);
             }
         });
 
@@ -31,6 +58,26 @@ public class LoginActivity extends AppCompatActivity {
         sign_up.setOnClickListener(new View.OnClickListener(){
             public void onClick(View view){
                 startActivity(new Intent(LoginActivity.this, SignUpActivity.class));
+            }
+        });
+    }
+    //lets user log in
+    private void login(String username, String password){
+        ParseUser.logInInBackground(username, password, new LogInCallback(){
+            @Override
+            public void done(ParseUser user, ParseException e){
+                if (e == null){
+                    Log.d("LoginActivity", "Login successful!");
+
+                    final Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+                else{
+                    Log.e("LoginActivity", "Login Failure");
+                    Toast.makeText(LoginActivity.this,"Login Failed",  Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
+                }
             }
         });
     }
--- app/src/main/java/me/leojlindo/travelbud/SignUpActivity.java
+++ app/src/main/java/me/leojlindo/travelbud/SignUpActivity.java
@@ -3,10 +3,26 @@ package me.leojlindo.travelbud;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    private EditText firstnameInput;
+    private EditText lastnameInput;
+    private EditText usernameInput;
+    private EditText emailInput;
+    private EditText phoneInput;
+    private EditText passwordInput;
+    private Button signupBtn;
+
 
 
     @Override
@@ -14,16 +30,60 @@ public class SignUpActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
-        // hides the action bar
-        //getSupportActionBar().hide();
+        firstnameInput = findViewById(R.id.firstname_tv);
+        lastnameInput = findViewById(R.id.lastname_tv);
+        usernameInput = findViewById(R.id.user_tv);
+        emailInput = findViewById(R.id.email_tv);
+        phoneInput = findViewById(R.id.phone_tv);
+        passwordInput = findViewById(R.id.password_tv);
+        signupBtn = findViewById(R.id.Sign_up_btn);
 
 
         // connecting sign up button to the main page after signing up
-        Button sign_up_btn = (Button) findViewById(R.id.Sign_up_btn);
-
-        sign_up_btn.setOnClickListener(new View.OnClickListener(){
+        signupBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
             public void onClick(View view){
-                startActivity(new Intent(SignUpActivity.this, MainActivity.class));
+                final String username = usernameInput.getText().toString();
+                final String password = passwordInput.getText().toString();
+                final String email = emailInput.getText().toString();
+
+                signUp(view);
+            }
+        });
+    }
+    // creating a new user and sending the data to the server
+    private void signUp(View view){
+        firstnameInput = findViewById(R.id.firstname_tv);
+        lastnameInput = findViewById(R.id.lastname_tv);
+        usernameInput = findViewById(R.id.user_tv);
+        emailInput = findViewById(R.id.email_tv);
+        phoneInput = findViewById(R.id.phone_tv);
+        passwordInput = findViewById(R.id.password_tv);
+        signupBtn = findViewById(R.id.Sign_up_btn);
+
+        ParseUser user = new ParseUser();
+
+        // filling inputs for new user
+        user.setPassword(passwordInput.getText().toString());
+        user.setUsername(usernameInput.getText().toString());
+        user.setEmail(emailInput.getText().toString());
+        user.put("firstName", firstnameInput.getText().toString());
+        user.put("lastName", lastnameInput.getText().toString());
+        user.put("phoneNum",phoneInput.getText().toString());
+
+        user.signUpInBackground(new SignUpCallback() {
+            public void done(ParseException e) {
+                if (e == null) {
+                    Log.d("SignUp", "Sign Up Success");
+                    final Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+
+                } else {
+                    Log.d("SignUp", "Sign Up Failed");
+                    Toast.makeText(SignUpActivity.this,"Sign Up Failed",  Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
+                }
             }
         });
     }
