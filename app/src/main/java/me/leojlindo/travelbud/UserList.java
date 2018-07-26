cf0607dcb8d2735bddb1e0c6f7bfe15e7aee9983
Leonardo Lindo <leojlindo@fb.com>
2018-07-25 18:01:13 -0700

    added xml and java files for the users lists

---
+++ app/src/main/java/me/leojlindo/travelbud/UserList.java
@@ -0,0 +1,16 @@
+package me.leojlindo.travelbud;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UserList extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_list);
+    }
+
+
+}
